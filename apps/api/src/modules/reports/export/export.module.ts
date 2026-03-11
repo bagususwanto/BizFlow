1d4ae53e27f5a7d8d6fce33fde510b481b204bf8
@bagus_uswanto <bagoes.1933@gmail.com>
2026-03-11 17:18:46 +0700

    feat: Add StockValuationModule to ExportModule imports.

--- apps/api/src/modules/reports/export/export.module.ts
+++ apps/api/src/modules/reports/export/export.module.ts
@@ -3,10 +3,11 @@ import { ExportController } from './export.controller';
 import { ExportService } from './export.service';
 import { SalesReportModule } from '../sales';
 import { StockReportModule } from '../inventory';
+import { StockValuationModule } from '../../inventory/stock-valuation/stock-valuation.module';
 import { PrismaModule } from '../../../prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule, SalesReportModule, StockReportModule],
+  imports: [PrismaModule, SalesReportModule, StockReportModule, StockValuationModule],
   controllers: [ExportController],
   providers: [ExportService],
   exports: [ExportService],
